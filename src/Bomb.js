1c1af57ae48291d5d6e7b8d2ffa052654fc4ed48
Eva <yi.zheng1709@gmail.com>
2021-05-11 22:38:19 -0400

    commit tree a143aaae06ea8a1556080bff802918fe9ad43533

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
+// const { Component } = require("react");
+import React, { Component } from 'react';
+
 // your Bomb code here!
+class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+    render(){
+        if (this.state.secondsLeft > 0){
+        return (
+            <div>
+                {this.state.secondsLeft} seconds left before I go boom!
+            </div>
+        )} else {
+            return (
+                <div> 
+                    Boom!
+                </div>
+            )
+        }
+    }
+}
+
+export default Bomb
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,27 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+
+    }
+    increment() {
+        let newCount = this.state.count + 1
+        this.setState({
+            count: newCount
+        })
+    }
+    render() {
+        return (
+            <div>
+                I am on slide {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
+
+export default ImageSlider
